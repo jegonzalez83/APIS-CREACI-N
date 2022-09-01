91d620babcd1ee6a730204945bda334050f36324
Jhon González <enriquemime@gmail.com>
2022-09-01 11:40:43 -0300

    cambios para subir a git hub

---
+++ public/js/moviesAddValidation.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    let titulo = document.querySelector('.moviesAddTitulo')
+    let formulario = document.querySelector('#formulario');
+    let article = document.querySelector('article');
+    titulo.innerHTML = 'AGREGAR PELÍCULA';
+    titulo.classList.add('titulo');
+    article.classList.add('fondoTransparente');
+    formulario.classList.add('fondoCRUD');
+
+//------DESDE AQUÍ SE DEBE CONTINUAR CON LAS VALIDACIONES DEL FORMULARIO DE REGISTRO DE PELÍCULAS//
+   
+
+}
